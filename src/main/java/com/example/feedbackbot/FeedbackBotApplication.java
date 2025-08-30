6a2a950c38d85e3b983b855a7c71a070995159d9
Игорь Царёв <tsarov88@gmail.com>
2025-08-30 19:52:20 +0300

    Initial commit

---
+++ src/main/java/com/example/feedbackbot/FeedbackBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.feedbackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackBotApplication.class, args);
+    }
+
+}
